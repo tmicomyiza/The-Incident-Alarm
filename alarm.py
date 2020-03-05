e2541c325784badb5520e0de1bcfe9edcba0cefb
root <root@kali>
2020-03-04 23:47:59 -0500

    password

--- alarm.py
+++ alarm.py
@@ -10,31 +10,32 @@ Protocols = ["HOPOPT", "ICMP", "IGMP", "GGP", "IPv4", "ST", "TCP", "CBT", "EGP",
 "ARGUS", "EMCON", "XNET", "CHAOS", ]
 count = 0
 
+
 def packetcallback(packet):
   global count
 
   try:
     if packet[TCP].dport == 3389:
       count = count + 1
-      print("ALERT #{}: RDP is detected from {} {}!" .format(count, packet[IP].src, Protocols[packet.proto]))
+      print("ALERT #{}: RDP is detected from {} ({})!" .format(count, packet[IP].src, Protocols[packet.proto]))
 
     if packet[TCP].flags == "": #null scan
       count = count + 1
-      print("ALERT #{}: Null scan is detected from {} {}!" .format(count, packet[IP].src, Protocols[packet.proto]))
+      print("ALERT #{}: Null scan is detected from {} ({})!" .format(count, packet[IP].src, Protocols[packet.proto]))
     
     if packet[TCP].flags == "F": # fin scan
       count = count + 1
-      print("ALERT #{}: Fin scan is detected from {} {}!" .format(count, packet[IP].src, Protocols[packet.proto]))
+      print("ALERT #{}: Fin scan is detected from {} ({})!" .format(count, packet[IP].src, Protocols[packet.proto]))
 
     if packet[TCP].flags == "FPU": # xmas scan
       count = count + 1
-      print("ALERT #{}: Xmas scan is detected from {} {}!" .format(count, packet[IP].src, Protocols[packet.proto]))
+      print("ALERT #{}: Xmas scan is detected from {} ({})!" .format(count, packet[IP].src, Protocols[packet.proto]))
     
 
     packet_data_raw = str(packet)      
     if "nikto" in packet_data_raw.lower(): # Nikto scan
       count = count + 1
-      print("ALERT #{}: Nikto scan is detected from {} {}!" .format(count, packet[IP].src, Protocols[packet.proto]))
+      print("ALERT #{}: Nikto scan is detected from {} ({})!" .format(count, packet[IP].src, Protocols[packet.proto]))
     
 
   except:
@@ -48,7 +49,13 @@ if args.pcapfile:
   try:
     print("Reading PCAP file %(filename)s..." % {"filename" : args.pcapfile})
     sniff(offline=args.pcapfile, prn=packetcallback)
-    os.system('ettercap -T -r ' + args.pcapfile + ' | grep \"PASS:\"')
+
+    #capture passwords and username
+    passuser = os.popen('ettercap -T -r ' + args.pcapfile + ' | grep \"PASS:\"').read()
+
+    count = count + 1
+    print("ALERT #{}: Password and username detected {}" .format(count,passuser))
+
   except:
     print("Sorry, something went wrong reading PCAP file %(filename)s!" % {"filename" : args.pcapfile})
 else:
